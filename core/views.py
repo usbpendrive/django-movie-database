38ced2962d4f1b4cc7f305fe077e1fea451cbaf8
usbpendrive <ferry.fernando@gmail.com>
2019-01-09 09:42:21 +0700

    Objects API cache strategy

--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
+import django
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.cache import cache
 from django.core.exceptions import PermissionDenied
 from django.views.generic import (
     CreateView, DetailView, ListView, UpdateView
@@ -128,4 +130,15 @@ class MovieImageUpload(LoginRequiredMixin, CreateView):
 
 class TopMovies(ListView):
     template_name = 'core/top_movies_list.html'
-    queryset = Movie.objects.top_movies(limit=10)
+
+    def get_queryset(self):
+        limit = 10
+        key = 'top_movies_%s' % limit
+        cached_qs = cache.get(key)
+        if cached_qs:
+            same_django = cached_qs._django_version == django.get_version()
+            if same_django:
+                return cached_qs
+        qs = Movie.objects.top_movies(limit=limit)
+        cache.set(key, qs)
+        return qs
